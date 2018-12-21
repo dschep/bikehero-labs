ebbf7203667b668e3de82d5dda6d8489d2f88691
Daniel Schep <dschep@gmail.com>
2018-12-21 17:30:35 -0500

    archive lyft,bird&skip

--- dobi-archiver/bikefinder/scraper.py
+++ dobi-archiver/bikefinder/scraper.py
@@ -51,6 +51,30 @@ def scrape_spin(event, context):
 
 @no_retry_on_failure
 @database
+def scrape_bird(event, context):
+    resp = requests.get('https://gbfs.bird.co/dc')
+    bikes = Bikes.from_gbfs_json(resp.json(), 'bird')
+    save_to_db(bikes, context.db)
+    return bikes.geojson
+
+@no_retry_on_failure
+@database
+def scrape_lyft(event, context):
+    resp = requests.get('https://s3.amazonaws.com/lyft-lastmile-production-iad/lbs/dca/free_bike_status.json')
+    bikes = Bikes.from_gbfs_json(resp.json(), 'lyft')
+    save_to_db(bikes, context.db)
+    return bikes.geojson
+
+@no_retry_on_failure
+@database
+def scrape_skip(event, context):
+    resp = requests.get('https://us-central1-waybots-production.cloudfunctions.net/dcFreeBikeStatus')
+    bikes = Bikes.from_gbfs_json({'data': resp.json()}, 'skip')
+    save_to_db(bikes, context.db)
+    return bikes.geojson
+
+@no_retry_on_failure
+@database
 def scrape_mobike_gbfs(event, context):
     resp = requests.get('https://mobike.com/us/gbfs/v1/free_bike_status')
     bikes = Bikes.from_gbfs_json(resp.json(), 'mobike')
